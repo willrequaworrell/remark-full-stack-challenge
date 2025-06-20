dfc5d8d19aabf3935847a4732993fbf74c130f6f
Will Worrell <willrequaworrell@gmail.com>
2025-06-20 14:56:48 -0400

    lint appeasement

--- src/app/components/PlaylistPicker.tsx
+++ src/app/components/PlaylistPicker.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { LoadingSpinner } from "./LoadingSpinner";
 import { SpotifyPlaylist } from "../types/spotify";
