e894922570709526ad054d323ca86027856534b6
Will Worrell <willrequaworrell@gmail.com>
2025-06-27 12:54:21 -0400

    import chat section skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import {
   PlaylistTrackCardType,
   SpotifyPlaylistTrack
 } from "./types/track";
+import ChatSectionSkeleton from "./components/ChatSectionSkeleton";
 
 export default function Page() {
   const [selectedPlaylist, setSelectedPlaylist] = useState<string | null>(null);
@@ -155,14 +156,7 @@ export default function Page() {
             aiConsolidatedTrackData={aiConsolidatedTrackData}
           />
         ) : (
-          <div className="flex flex-col lg:w-[38.2%] w-full p-4 md:p-6 h-full border-l-0">
-            <div className="flex items-center justify-between mb-2">
-              <h2 className="text-lg font-black md:text-xl">
-                AI Disc Jockey
-              </h2>
-            </div>
-            <LoadingSpinner size={8} title="Analyzing Playlist..." />
-          </div>
+          <ChatSectionSkeleton/>
         )}
       </div>
     </div>
